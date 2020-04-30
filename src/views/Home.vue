c86d6b52bda02ef41b4a9df7e1a8eb573ab1b333
Stanko Bebek <stanko.bebek82@gmail.com>
2020-05-01 00:55:30 +0200

    added school table view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,68 @@
 <template>
   <div class="home">
-    aaa
+    <div class="container">
+
+      <!--Lista svih škola-->
+      <h1>Popis svih škola</h1>
+      <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">
+            ID škole
+          </th>
+          <th scope="col">
+            Naziv škole
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="school in schools" :key="school.id" scope="row">
+          <td>{{ school.skola_id }}</td>
+          <td>{{ school.naziv }}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
   </div>
 </template>
 
 <script>
+import db from '../components/FirebaseInit'
 export default {
   name: 'Home',
-  components: {
+  data(){
+    return{
+      students: [],
+      schools: []
+    }
+  },
+  created(){
+    db.collection('ucenici').get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          'id':doc.id,
+          'ucenik_id': doc.data().ucenik_id,
+          'ime': doc.data().ime,
+          'prezime': doc.data().prezime,
+          'godine': doc.data().godine,
+          'grad': doc.data().grad,
+          'skola': doc.data().skola
+        }
+        this.students.push(data)
+      })
+    })
+
+    db.collection('skole').get().then( querySnapshot =>{
+      querySnapshot.forEach(doc =>{
+        const data = {
+          'id': doc.id,
+          'skola_id': doc.data().skola_id,
+          'naziv': doc.data().naziv
+        }
+        this.schools.push(data)
+      })
+    })
   }
 }
 </script>
